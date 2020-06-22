1c17ea48443557bbb6d7f49df643ca1f84e8dca0
Gustavo Mancuzo <gustavosmanc@gmail.com>
2020-06-22 01:23:21 -0300

    Commit - QForecast

---
+++ scripts/app.js
@@ -0,0 +1,50 @@
+const form = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateCity = async (city) => {
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+
+  return { cityDets, weather };
+};
+
+const updateUI = (data) => {
+  const { cityDets, weather } = data;
+
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+
+  const timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+
+  time.setAttribute('src', timeSrc);
+
+  iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const city = form.city.value.trim();
+  form.reset();
+
+  updateCity(city)
+    .then((data) => {
+      console.log(data);
+      updateUI(data);
+    })
+    .catch((err) => console.log(err));
+});
